770fdfdfd5bbcea4df43d2b47715502d5c96e936
Josefine Lantz <jo.lantz@icloud.com>
2024-04-17 12:52:28 +0200

    fix

--- sql_app/main.py
+++ sql_app/main.py
@@ -21,14 +21,14 @@ async def root():
   return {"message": "Hello World"}
     
 @app.post("/articles/", response_model=schemas.Article)
-def create_article(article: schemas.ArticleCreate, db: Session = Depends(get_db)):
+async def create_article(article: schemas.ArticleCreate, db: Session = Depends(get_db)):
   db_article = crud.get_article_by_title(db, title=article.title)
   if db_article:
     raise HTTPException(status_code=400, detail="Article already registered")
   return crud.create_article(db=db, article=article)
 
 @app.get("/articles", response_model=list[schemas.Article])
-def read_articles(skip: int = 0, limit: int= 100, db: Session = Depends(get_db)):
+async def read_articles(skip: int = 0, limit: int= 100, db: Session = Depends(get_db)):
   articles = crud.get_articles(db, skip=skip, limit=limit)
   return articles 
   
